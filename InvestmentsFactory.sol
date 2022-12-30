65040ecc0496b9bc49f69b24505e40c14791b354
sofian-eth <94702305+sofian-eth@users.noreply.github.com>
2022-12-30 23:18:09 +0500

    Update InvestmentsFactory.sol

--- InvestmentsFactory.sol
+++ InvestmentsFactory.sol
@@ -36,6 +36,7 @@ contract InvestmentsFactory {
         uint256 minInvestInWei;
         uint256 openTime;
         uint256 closeTime;
+        bool fixedPresale;
     }
 
     struct PresaleUniswapInfo {
@@ -85,7 +86,8 @@ contract InvestmentsFactory {
             _info.maxInvestInWei,
             _info.minInvestInWei,
             _info.openTime,
-            _info.closeTime
+            _info.closeTime,
+            _info.fixedPresale
         );
         _presale.setUniswapInfo(
             _uniInfo.listingPriceInWei,
@@ -100,9 +102,9 @@ contract InvestmentsFactory {
     function createPresale(
         PresaleInfo calldata _info,
         PresaleUniswapInfo calldata _uniInfo
-    ) external //payable 
+    ) external payable 
     {
-        //require(msg.value == 0.01 ether, "msg.value less than 2.5 BNB. Send 2.5 BNB to create presale.");
+        require(msg.value == 0.01 ether, "msg.value less than 0.01 Matic. Send 0.01 Matic to create presale.");
         IERC20 token = IERC20(_info.tokenAddress);
 
         InvestmentsPresale presale = new InvestmentsPresale(address(this), SSS.owner());
@@ -131,6 +133,41 @@ contract InvestmentsFactory {
         presale.setInfo(address(liquidityLock), Id);
 
         emit PresaleCreated(Id, address(presale), address(liquidityLock));
-        //payable(SSS.owner()).transfer(msg.value);
+        payable(SSS.owner()).transfer(msg.value);
+    }
+
+    function createPresaleP(
+        PresaleInfo calldata _info,
+        PresaleUniswapInfo calldata _uniInfo
+    ) external 
+    {
+        IERC20 token = IERC20(_info.tokenAddress);
+
+        InvestmentsPresale presale = new InvestmentsPresale(address(this), SSS.owner());
+
+        address existingPairAddress = QuickSwapFactory.getPair(address(token), wmaticAddress);
+        require(existingPairAddress == address(0), "Pair already exists"); // token should not be listed in PancakeSwap
+
+        uint256 maxEthPoolTokenAmount = _info.hardCapInWei.mul(_uniInfo.liquidityPercentageAllocation).div(100);
+        uint256 maxLiqPoolTokenAmount = maxEthPoolTokenAmount.mul(1e18).div(_uniInfo.listingPriceInWei);
+
+        uint256 maxTokensToBeSold = _info.hardCapInWei.mul(1e18).div(_info.tokenPriceInWei);
+        uint256 requiredTokenAmount = maxLiqPoolTokenAmount.add(maxTokensToBeSold);
+        token.transferFrom(msg.sender, address(presale), requiredTokenAmount);
+
+        initializePresale(presale, requiredTokenAmount, maxTokensToBeSold, _info.tokenPriceInWei, _info, _uniInfo);
+
+        address pairAddress = uniV2LibPairFor(address(QuickSwapFactory), address(token), wmaticAddress);
+        InvestmentsLiquidityLock liquidityLock = new InvestmentsLiquidityLock(
+                IERC20(pairAddress),
+                msg.sender,
+                address(presale),
+                _uniInfo.liquidityAddingTime + (_uniInfo.lpTokensLockDurationInDays * 1 days)
+            );
+
+        uint256 Id = SSS.addPresaleAddress(address(presale));
+        presale.setInfo(address(liquidityLock), Id);
+
+        emit PresaleCreated(Id, address(presale), address(liquidityLock));
     }
 }
