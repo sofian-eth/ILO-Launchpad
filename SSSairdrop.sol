79fca8ad66ff069ae86b8dda12fcc53a4d381a96
sofian-eth <94702305+sofian-eth@users.noreply.github.com>
2023-01-25 19:03:15 +0500

    Update SSSairdrop.sol

--- SSSairdrop.sol
+++ SSSairdrop.sol
@@ -12,15 +12,20 @@ contract SSSairdrop{
     uint256 public totalParticipants;
     address public tokenAddress;
     uint256 public distributionTime;
+    uint256 private totalTokens;
+    uint256 public quantityPerUser;
 
     IERC20 private token;
 
-    constructor (address _token, address _airdropCreator, uint256 _distributionTime) public {
+    bool public airdropped = false;
+
+    constructor (address _token, address _airdropCreator, uint256 _distributionTime, uint256 _totalTokens) public {
         require(_distributionTime > block.timestamp, "Error: distibution time is before current time");
         token = IERC20(_token);
         airdropCreator = _airdropCreator;
         tokenAddress = _token;
         distributionTime = _distributionTime;
+        totalTokens = _totalTokens;
     }
 
     mapping(address=>bool) public participated;
@@ -38,6 +43,7 @@ contract SSSairdrop{
 
     function participate() external alreadyParticipant {
         require(block.timestamp < distributionTime, "Sorry you're late, you cannot participate at this time");
+        require(!airdropped, "Tokens already airdropped, can't participate now");
         participated[msg.sender] = true;
 
         participants.push(msg.sender);
@@ -46,11 +52,28 @@ contract SSSairdrop{
 
     function airdropUsers() external onlyAirdropCreator {
         require(block.timestamp > distributionTime, "Distribution time hasn't been reached yet");
+        require(!airdropped, "Tokens already airdropped");
+        airdropped = true;
         uint256 balance = token.balanceOf(address(this)) / totalParticipants;
+        quantityPerUser = balance;
 
         uint256 loops = participants.length - 1;
         for(uint i = 0; i <= loops; i++) {
             token.transfer(participants[i], balance);
+        }
+    }
+
+    function tokens() external view returns (uint256) {
+        return totalTokens;
+        //return totalTokens;
+    }
+
+    function checkStatus() external view returns (bool) {
+        if(block.timestamp > distributionTime) {
+            return true;
+        }
+        else {
+            return false;
         }
     }
 }
